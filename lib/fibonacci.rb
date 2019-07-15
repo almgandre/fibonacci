5a20fdd6eaaf21cfe89d39a80eba587ec4a5f2ab
Andre Barbosa <almgandre@gmail.com>
2019-07-15 13:57:59 +0100

    Rename var

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,23 +1,23 @@
 class Fibonacci
-  attr_reader :limit, :sequence
+  attr_reader :amount, :sequence
 
-  DEFAULT_LIMIT = 100
+  DEFAULT_AMOUNT = 100
   INITIAL_NUMBERS = [0, 1].freeze
 
-  def initialize(limit=DEFAULT_LIMIT)
-    @limit = limit
+  def initialize(amount=DEFAULT_AMOUNT)
+    @amount = amount
     @sequence = INITIAL_NUMBERS.dup
   end
 
   def call
-    sequence << sum_of_last_2_numbers while within_limit
+    sequence << sum_of_last_2_numbers while within_amount
     sequence
   end
 
   private
 
-  def within_limit
-    sequence.count < limit
+  def within_amount
+    sequence.count < amount
   end
 
   def sum_of_last_2_numbers
